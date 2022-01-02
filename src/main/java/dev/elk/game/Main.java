cdd54e6d6504f8f52578d47488acf66a951cc55b
Rechenmaschine <louis.schell1@gmx.de>
2022-01-02 22:09:31 +0100

    Moved assets into "assets" folder.

---
+++ src/main/java/dev/elk/game/Main.java
@@ -0,0 +1,28 @@
+package dev.elk.game;
+
+import dev.elk.scaffold.components.Window;
+import dev.elk.scaffold.renderer.*;
+import dev.elk.scaffold.renderer.Shader.ShaderType;
+
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * Start game here. In this case, a demonstration of the recently
+ * implemented vertex object.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Shader fragment = new Shader("Assets/Shaders/fragment.glsl", ShaderType.FRAGMENT);
+        Shader vertex = new Shader("Assets/Shaders/vertex.glsl", ShaderType.VERTEX);
+        ShaderProgram program = new ShaderProgram();
+        program.attachShaders(fragment, vertex);
+
+        Window window = new Window("Hello!", 500, 500, Color.black);
+        window.setScene(new PrimaryScene(program));
+
+        window.run();
+    }
+}
--- src/main/java/dev/elk/scaffold/Main.java
+++
@@ -1,8 +0,0 @@
-package dev.elk.scaffold;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}

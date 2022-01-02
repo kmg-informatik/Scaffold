9d5f2b47e2e3ec69ad1022269e9d60934a3a6987
Rechenmaschine <louis.schell1@gmx.de>
2022-01-02 22:12:09 +0100

    Added Listeners for user input.

---
+++ src/main/java/dev/elk/scaffold/components/KeyListener.java
@@ -0,0 +1,30 @@
+package dev.elk.scaffold.components;
+
+import java.util.HashSet;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+/**
+ * The Key-Listener listens to button-presses on the keyboard.
+ * Original code from GamesWithGabe, heavily modified.
+ * @author Louis Schell
+ */
+public class KeyListener {
+    private static final HashSet<Integer> keyPressed = new HashSet<>();
+
+    private KeyListener() {}
+
+    public static void keyCallback(long window, int key, int scancode, int action, int mods) {
+        if (action == GLFW_PRESS) {
+            keyPressed.add(key);
+        } else if (action == GLFW_RELEASE) {
+            keyPressed.remove(key);
+        }
+    }
+
+    public static boolean isKeyPressed(int keyCode) {
+        return keyPressed.contains(keyCode);
+    }
+
+}
---
+++ src/main/java/dev/elk/scaffold/components/MouseListener.java
@@ -0,0 +1,87 @@
+package dev.elk.scaffold.components;
+
+import java.util.HashSet;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+/**
+ * The Mouse-listener listens to actions taken by the mouse
+ * such as button clicks or moving of the mouse. Original code
+ * from GamesWithGabe, heavily modified.
+ * @author Louis Schell
+ */
+public class MouseListener {
+
+    private static final HashSet<Integer> buttonsPressed = new HashSet<>(3);
+    private static double scrollX, scrollY;
+    private static double xPos, yPos, lastX, lastY;
+    private static boolean isDragging;
+
+    private MouseListener(){}
+
+    public static void mousePosCallback(long window, double newXPos, double newYPos) {
+        lastX = xPos;
+        lastY = yPos;
+        xPos = newXPos;
+        yPos = newYPos;
+        isDragging = !buttonsPressed.isEmpty();
+    }
+
+    public static void mouseButtonCallback(long window, int button, int action, int mods) {
+        if (action == GLFW_PRESS) {
+            buttonsPressed.add(button);
+        } else if (action == GLFW_RELEASE) {
+            buttonsPressed.remove(button);
+            isDragging = false;
+        }
+    }
+
+    public static void mouseScrollCallback(long window, double xOffset, double yOffset) {
+        scrollX = xOffset;
+        scrollY = yOffset;
+    }
+    
+    public static void reset(){
+        scrollX = 0;
+        scrollY = 0;
+        xPos = 0;
+        yPos = 0;
+        lastX = 0;
+        lastY = 0;
+        buttonsPressed.clear();
+        isDragging = false;
+    }
+
+    public static boolean isPressed(int button){
+        return buttonsPressed.contains(button);
+    }
+
+    public static double getScrollX() {
+        return scrollX;
+    }
+
+    public static double getScrollY() {
+        return scrollY;
+    }
+
+    public static double getXPos() {
+        return xPos;
+    }
+
+    public static double getYPos() {
+        return yPos;
+    }
+
+    public static double getLastX() {
+        return lastX;
+    }
+
+    public static double getLastY() {
+        return lastY;
+    }
+
+    public static boolean isDragging() {
+        return isDragging;
+    }
+}

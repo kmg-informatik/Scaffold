75177b053c90e94d46b9dac57040418c44061278
Louis Schell <louis.schell1@gmx.de>
2022-01-09 21:40:45 +0100

    Fixed vectors not being newly created and chained constructors in Quad.java

--- src/main/java/dev/elk/scaffold/gl/Quad.java
+++ src/main/java/dev/elk/scaffold/gl/Quad.java
@@ -17,23 +17,12 @@ public class Quad implements Geometry{
     private final Vertex[] vertices = new Vertex[4];
     private final Sprite sprite;
 
-
-    public Quad(Sprite sprite, Vector2f posLB, float width, float height){
-        this.sprite = sprite;
-        vertices[0] = new Vertex(new Vector2f(posLB), new Vector3f(), sprite.getUvCoords()[0]);
-        vertices[1] = new Vertex(posLB.add(width, 0), new Vector3f(), sprite.getUvCoords()[1]);
-        vertices[2] = new Vertex(posLB.add(0, height),new Vector3f(),  sprite.getUvCoords()[2]);
-        vertices[3] = new Vertex(posLB.add(width, height),new Vector3f(),  sprite.getUvCoords()[3]);
+    public Quad(Sprite sprite, Vector2f posLB, Vector2f posTR){
+        this(sprite, posLB, posTR, Color.WHITE);
     }
 
-    public Quad(Sprite sprite, Vector2f posLB, Vector2f posTR){
-        Color color = Color.WHITE;
-        Vector3f colorVec = new Vector3f(color.getRGBColorComponents(null));
-        this.sprite = sprite;
-        vertices[0] = new Vertex(new Vector2f(posLB),colorVec,  sprite.getUvCoords()[0]);
-        vertices[1] = new Vertex(new Vector2f(posTR.x, posLB.y), colorVec, sprite.getUvCoords()[1]);
-        vertices[2] = new Vertex(new Vector2f(posTR), colorVec, sprite.getUvCoords()[2]);
-        vertices[3] = new Vertex(new Vector2f(posLB.x, posTR.y),colorVec,  sprite.getUvCoords()[3]);
+    public Quad(Sprite sprite, Vector2f posLB, float width, float height){
+        this(sprite, posLB, new Vector2f(posLB).add(width, height));
     }
 
     public Quad(Sprite sprite, Vector2f posLB, Vector2f posTR, Color color) {

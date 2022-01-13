ca38c07aa011c7e9dec5d8c4d0a7c0dbcc2f6a7f
Rechenmaschine <louis.schell1@gmx.de>
2022-01-13 11:38:02 +0100

    Commented PipeChunk

--- src/main/java/dev/elk/game/structures/PipeChunk.java
+++ src/main/java/dev/elk/game/structures/PipeChunk.java
@@ -4,8 +4,17 @@ import dev.elk.scaffold.physics.CollidableStructure;
 
 import java.util.List;
 
+/**
+ * A pipe chunk is a {@link Chunk} that generates pipes. Chunks are generated
+ * procedurally, so that we can have an infinite amount.
+ * @author Felix Kunze
+ * @author Louis Schell
+ */
 public class PipeChunk extends Chunk implements CollidableStructure {
 
+    /**
+     * The latest chunk that was generated
+     */
     private Chunk lastChunk;
 
     public PipeChunk(float position, Chunk lastChunk) {
@@ -15,6 +24,15 @@ public class PipeChunk extends Chunk implements CollidableStructure {
         this.lastChunk = null; //Clear reference for gc
     }
 
+    /**
+     * Generates pipes by performing a random walk. This is done,
+     * so that the pipe gap is not too far away from the last pipe gap
+     * and is passable by the player.<br>
+     * The pipe gap width changes with the difficulty, which changes
+     * with the pipe count, which counts how many pipes the player
+     * has passed.
+     * @apiNote Expand on usage of difficulty
+     */
     public void generatePipes() {
         float previousPos = lastChunk.getQuads()[1].getMaxX()-lastChunk.getQuads()[1].getMinX();
 
@@ -33,6 +51,10 @@ public class PipeChunk extends Chunk implements CollidableStructure {
         }
     }
 
+    /**
+     * Gets the gap position, limiting it the range [-7,7] because
+     * else it would go out of the screen.
+     */
     public float setGapPosition(float previous, float difficulty) {
         int sign = random.nextBoolean()?1:-1;
         float pos = previous + sign*random.nextFloat()*3f;
